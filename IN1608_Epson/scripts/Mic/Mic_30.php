98dc11bf1347e71e4cb6fd82426acdc3052abd50
Indra-Irvin <47837340+Indra-Irvin@users.noreply.github.com>
2024-01-10 10:57:34 -0500

    Delete IN1608_Epson directory

--- IN1608_Epson/scripts/Mic/Mic_30.php
+++
@@ -1,7 +0,0 @@
-<?php
-$command=escapeshellcmd(" python Mic_30.py");
-$output=shell_exec($command);
-//echo $output;
-header("Location: ../projector.html");
-die();
-?>
--- IN1608_Epson/scripts/Mic/Mic_40.php
+++
@@ -1,7 +0,0 @@
-<?php
-$command=escapeshellcmd(" python Mic_40.py");
-$output=shell_exec($command);
-//echo $output;
-header("Location: ../projector.html");
-die();
-?>
